8ca155cf0a8cd6c572c05650419b7bfc2f7561b5
Xurmein <wj.pavocrist@gmail.com>
2018-12-20 16:44:19 -0500

    yelp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { YelpSearchService } from './yelp-search.service';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'yelp-api';
+export class AppComponent { 
+  title = 'FeedMeNOW!';
+  search : FormGroup
+  results : Results
+ 
+ 
+    
+  constructor(
+    private fb : FormBuilder,
+    private yelp : YelpSearchService
+  ) { }
+
+  ngOnInit(){this.search = this.fb.group({
+    zip: new FormControl(),
+    price: new FormControl()
+    })
+  }
+  
+  async foodMe() {
+    await this.yelp.getFoods(
+      event,
+      this.search.value.price,
+      this.search.value.zip
+    ).subscribe(food => 
+      {this.results = food.businesses[Math.floor(Math.random() * 15)]
+    })
+  }
 }
+
+export class Results{
+  results : any
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { YelpSearchService } from './yelp-search.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+    ],
+  providers: [YelpSearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
